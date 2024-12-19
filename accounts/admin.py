26612a276a99799eb2f43ed2e346f6c9a6223cfd
David g <jh0369549@gmail.com>
2024-12-19 14:31:14 +0330

    setup custom user model in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin 
+from .models import User 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 # Register your models here.
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta :
+        models = User
+        fields = ['email']
+        
+class CustomUserChengForm(UserChangeForm):
+    class Meta:
+        models = User
+        fields = ['email']         
+    
+
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChengForm
+    
+    model = User
+    list_display = ['email','is_superuser', 'is_active' ]
+    list_filter = ['email', 'is_active']
+    search_fields = ['email', 'is_superuser']
+    ordering = ['is_active']
+    
+    fieldsets = (
+        ('authentication', {
+            "fields": (
+                
+                'email' , 'password'
+            ),
+        }),
+          ('important dates', {
+            "fields": (
+                
+                'last_login' ,
+            ),
+        }),
+          ('group permissions', {
+            "fields": (
+                
+                'groups' , 'user_permissions'
+            ),
+        }),
+              ('permissions', {
+            "fields": (
+                
+                'is_staff' , 'is_active', 'is_superuser'
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1","password2", "is_staff",
+                "is_active", "is_superuser"
+            )}
+        ),
+    )
+    
+    
+admin.site.register(User, CustomUserAdmin)    
