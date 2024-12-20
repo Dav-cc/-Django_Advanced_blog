559377c4eed4477134769e350524c3994b940121
David g <jh0369549@gmail.com>
2024-12-20 12:39:18 +0330

    make signal for create profile based on users

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager , AbstractBaseUser, PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class UserManager(BaseUserManager):
@@ -48,10 +50,10 @@ class User(AbstractBaseUser,PermissionsMixin):
         return self.email
     
 class Profile(models.Model):
-    User = models.ForeignKey(User, on_delete=models.CASCADE)    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
-    # image = models.ImageField(blank=True , null=True)
+    image = models.ImageField(blank=True , null=True)
     descreption = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date  = models.DateTimeField(auto_now_add=True)
@@ -59,3 +61,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.email
     
+@receiver(post_save, sender = User)    
+def save_profile(sender , instance,created, **kwargs):
+    if created :
+        Profile.objects.create(user = instance)
+        
