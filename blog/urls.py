ec268a7fe03ca536ca9a46bf064c8de0d21c2d6b
David g <jh0369549@gmail.com>
2024-12-22 15:33:46 +0330

    mmmmmmm.....

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path ,include
 from .views import indexview
 from django.views.generic import TemplateView
-
+from . import views
 urlpatterns = [
     path('fbvindex',indexview, name = 'fbv_test'),
-    path('cbvindex', TemplateView.as_view(template_name = 'base.html'))
+    # path('cbvindex', TemplateView.as_view(template_name = 'base.html', extra_context = {"name" : "ali"}))
+    path('cbvindex', views.HomeView.as_view() , name = 'cbv_index')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Post
 # Create your views here.
 
 def indexview(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', context = {"name" : "mmad"})
+
+class HomeView(TemplateView):
+    template_name = "base.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'ali'
+        context['posts'] = Post.objects.all()
+        return context
