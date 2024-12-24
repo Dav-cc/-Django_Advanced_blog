e9bce4cea16991a81b966f935ef0bafca2f0e797
David g <jh0369549@gmail.com>
2024-12-24 18:11:55 +0330

    cheng user as user model to Profile

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from accounts.models import Profile
 # Create your models here.
 
 class Category(models.Model):
@@ -14,12 +15,12 @@ class Post(models.Model):
     '''
     this is a class for define post for blog
     '''
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     content = models.TextField()
     image = models.ImageField(null=True, blank=True)
     status = models.BooleanField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField()
